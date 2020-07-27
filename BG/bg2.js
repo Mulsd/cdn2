516afad6ad01d6bcda296f33094ce0f3103a9f92
Mulsd <Mulsd@qq.com>
2020-07-27 20:27:20 +0800

    U

---
+++ BG/bg2.js
@@ -0,0 +1,13 @@
+var xiaokang = new xkTool();
+xiaokang.codeFull();
+xiaokang.bgPage();
+// 设置随机背景的图片
+xiaokang.imgList = [
+    "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/62fd3efba4e3d66c722416cc3c7c5ee2.png",
+    "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/0db95fb7e3bccb9ff012253a103e46fa.png",
+    "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/5eca656b76701a8bb7a6f237eaa54d00.png",
+    "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/220ca21ab67977f80763cfa0b739e7e8.png",
+
+];
+// 调用随机背景
+xiaokang.randomBg();
